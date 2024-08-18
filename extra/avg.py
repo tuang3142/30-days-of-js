f5de6e61e8d3037136ed6f27019f5bd676fd928a
dat-nnguyen <121501258+dat-nnguyen@users.noreply.github.com>
2024-08-18 14:41:54 +0700

    Update avg.py

--- extra/avg.py
+++ extra/avg.py
@@ -2,4 +2,4 @@ arr = [1,2,3,4,5,7]
 
 avg = sum(arr) / len(arr)
 
-print(avg) # this prints floating point number. can you make it an integer instead?
+print(int(avg)) # this prints floating point number. can you make it an integer instead?
