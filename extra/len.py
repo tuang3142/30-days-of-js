29ba238f543a305e546f4bd5da84e27b388fccb2
dat-nnguyen <121501258+dat-nnguyen@users.noreply.github.com>
2024-08-18 14:44:42 +0700

    Update len.py

--- extra/len.py
+++ extra/len.py
@@ -1,10 +1,11 @@
 def length(arr):
-    count = 1
+    count = 0 #change 1 to 0 
     for num in arr:
         count += 1
     return count
 
 arr = [1, 2, 3, 4, 5]
 
-# this should be 5 but it prints 6. can you find what is wrong and fix it?
+# this should be 5 but it prints 6(WHY IT PRINTS 6 INSTEAD OF 4). can you find what is wrong and fix it?
+# IT'S WORK
 print(length(arr))
